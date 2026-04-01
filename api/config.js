d36c30aa449b29009f1790d5ad3e68ec774c845e
mattjmosich <matt@mattmosich.com>
2026-03-31 17:00:30 -0700

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,8 @@
+module.exports = function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-store');
+  res.end(JSON.stringify({
+    supabaseUrl:     process.env.SUPABASE_URL      || '',
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || '',
+  }));
+};
